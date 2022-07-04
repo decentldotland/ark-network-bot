7ec60bb361b78f330d1d93152fe3a5e8434b117b
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-07-05 00:34:19 +0300

    feat: add ERC-compatible contracts
    experimental contracts - for testing purposes only.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
 // SWCs
-export const REGISTRY_ORACLE_ADDRESS = "RyapnVUDqgveGa1Claee16_ypyyoqPL0JM6etSo3EME";
-export const ARK_ORACLE_ADDRESS = "qP614umsvOo9Szvl-xqvnXH0xLOg2eKOsLYnKx2l5SA";
+// export const REGISTRY_ORACLE_ADDRESS = "RyapnVUDqgveGa1Claee16_ypyyoqPL0JM6etSo3EME";
+// export const ARK_ORACLE_ADDRESS = "qP614umsvOo9Szvl-xqvnXH0xLOg2eKOsLYnKx2l5SA";
+export const REGISTRY_ORACLE_ADDRESS = "ykcg3C-SZBorZvxZaoMoLRBl6YudqQEHygV7a8utaAQ";
+export const ARK_ORACLE_ADDRESS = "qzrkOILfbz7Y0F_yhwZQqnHje6BxSX7oehZYz35pKiM"; // initialized with dummy data
 export const REQUESTS_CACHE_CONTRACT_ADDRESS = "ltx8_wVIRdH5Vp30K8k9ouFZ2wsEFnXUmgo5oa-AwLQ";
 // BOT IDs
 export const BOT_ID = 5544680052;
