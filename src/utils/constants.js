8fb9f81e9da397b892feaffec3ca8b41a58a5e6c
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-07-12 21:40:53 +0300

    feat: upgrade contract addrs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,21 @@
 // SWCs
 // export const REGISTRY_ORACLE_ADDRESS = "RyapnVUDqgveGa1Claee16_ypyyoqPL0JM6etSo3EME";
 // export const ARK_ORACLE_ADDRESS = "qP614umsvOo9Szvl-xqvnXH0xLOg2eKOsLYnKx2l5SA";
-export const REGISTRY_ORACLE_ADDRESS = "ykcg3C-SZBorZvxZaoMoLRBl6YudqQEHygV7a8utaAQ";
+
+
+// export const REGISTRY_ORACLE_ADDRESS = "ykcg3C-SZBorZvxZaoMoLRBl6YudqQEHygV7a8utaAQ"; 000
+export const REGISTRY_ORACLE_ADDRESS = "R0tQjn7bUH28IhUElh4GwvNKw5aY9r-DF8Q5LjuzxO4";
+
+
 // export const ARK_ORACLE_ADDRESS = "qzrkOILfbz7Y0F_yhwZQqnHje6BxSX7oehZYz35pKiM"; // initialized with dummy data
-export const ARK_ORACLE_ADDRESS = "QV8nvqYpQ46wmFsgvmWPg4LqFkDoPOoyYnaxnxhQyxY" // TG verification migration compatible
+
+
+
+// export const ARK_ORACLE_ADDRESS = "QV8nvqYpQ46wmFsgvmWPg4LqFkDoPOoyYnaxnxhQyxY" // TG verification migration compatible 000
+
+export const ARK_ORACLE_ADDRESS = "jRGJtaBjfvDJgpQATUiW3mBbB_wp71xrUmeQBalrm3k"
+
+
 export const REQUESTS_CACHE_CONTRACT_ADDRESS = "ltx8_wVIRdH5Vp30K8k9ouFZ2wsEFnXUmgo5oa-AwLQ";
 // BOT IDs
 export const BOT_ID = 5544680052;
