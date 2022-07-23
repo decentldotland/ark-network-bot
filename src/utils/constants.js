f71d447f3fc989125ac53518f7885dfecf523a16
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-07-24 00:38:17 +0300

    feat: update SC addresses

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,9 +3,9 @@
 // export const ARK_ORACLE_ADDRESS = "qP614umsvOo9Szvl-xqvnXH0xLOg2eKOsLYnKx2l5SA";
 
 
-// export const REGISTRY_ORACLE_ADDRESS = "ykcg3C-SZBorZvxZaoMoLRBl6YudqQEHygV7a8utaAQ"; 000
-export const REGISTRY_ORACLE_ADDRESS = "R0tQjn7bUH28IhUElh4GwvNKw5aY9r-DF8Q5LjuzxO4";
-
+// export const REGISTRY_ORACLE_ADDRESS = "ykcg3C-SZBorZvxZaoMoLRBl6YudqQEHygV7a8utaAQ";
+// export const REGISTRY_ORACLE_ADDRESS = "R0tQjn7bUH28IhUElh4GwvNKw5aY9r-DF8Q5LjuzxO4"; 
+export const REGISTRY_ORACLE_ADDRESS = "sWR6h_DrFHBS2P2-l1zYFo0R7ufJy32YINM1UVP7f5w";
 
 // export const ARK_ORACLE_ADDRESS = "qzrkOILfbz7Y0F_yhwZQqnHje6BxSX7oehZYz35pKiM"; // initialized with dummy data
 
@@ -13,7 +13,7 @@ export const REGISTRY_ORACLE_ADDRESS = "R0tQjn7bUH28IhUElh4GwvNKw5aY9r-DF8Q5Ljuz
 
 // export const ARK_ORACLE_ADDRESS = "QV8nvqYpQ46wmFsgvmWPg4LqFkDoPOoyYnaxnxhQyxY" // TG verification migration compatible 000
 
-export const ARK_ORACLE_ADDRESS = "jRGJtaBjfvDJgpQATUiW3mBbB_wp71xrUmeQBalrm3k"
+export const ARK_ORACLE_ADDRESS = "VWWz1k2u6LnfNLJVCQxVVE3b2ivTVBbgLkMrPe3naoY"
 
 
 export const REQUESTS_CACHE_CONTRACT_ADDRESS = "ltx8_wVIRdH5Vp30K8k9ouFZ2wsEFnXUmgo5oa-AwLQ";
